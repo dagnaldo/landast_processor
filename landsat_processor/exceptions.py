ef79625855a1b598917499baacf4144f4aaaacf4
dagnaldo <naldo.penha@gmail.com>
2018-04-23 14:32:50 -0300

    Changes on excpetions for python3

--- landsat_processor/exceptions.py
+++ landsat_processor/exceptions.py
@@ -5,16 +5,14 @@
 class TMSError(Exception):
     """Class for TMS error exception for this module."""
 
-    def __init__(self, errors, *args, **kwargs):
-        super(TMSError, self).__init__(*args, **kwargs)
-        self.errors = errors
-    pass
+    def __init__(self, code, message):
+        super().__init__(message)
+        self.message = message
 
 
 class XMLError(Exception):
     """Class for XML error exception for this module."""
 
-    def __init__(self, errors, *args, **kwargs):
-        super(XMLError, self).__init__(*args, **kwargs)
-        self.errors = errors
-    pass
+    def __init__(self, code,message):
+        super().__init__(message)
+        self.message = message
